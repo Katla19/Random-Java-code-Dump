5e369fddc1890c2612c1ab529d6708c2b90f31a0
Katla19 <91776747+Katla19@users.noreply.github.com>
2022-06-22 05:06:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+class HelloWorld {
+    public static void main(String[] args) throws Exception {
+       /* BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("yenter");
+        char ch=(char)br.read();
+        System.out.println("sdfsd    "+ch);
+        Console c=System.console();
+        char[] str=c.readPassword();
+        System.out.println(str);
+        Scanner in =new Scanner(System.in);
+        char a=in.next().charAt(0);   
+        String str=in.nextLine();
+        System.out.println(a+str);
+        BufferedReader br =null;
+        br=new BufferedReader(new InputStreamReader(System.in));
+        String s= br.readLine(); //Input the number seperated by space
+        int[] arr= new int[8];
+        String[] s1 = s.split(" ");
+        for(int i=0;i<8;i++){
+         arr[i]=Integer.parseInt(s1[i]);
+        }
+        System.out.println(arr[0]);
+        for(int i=1;i<=3;i++){
+            for(int j=0;j<3-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for(int i=3-1;i>=1;i--){
+            for(int j=1;j<=3-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        char ch='a';
+        switch(ch){
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':System.out.println("dsdsd");break;
+            case 'g':
+            default:System.out.println("bb");break;
+        }
+        int a=10;
+        a=-1;
+        System.out.println(a);*/
+        
+    }
+}
